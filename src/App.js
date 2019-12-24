c5cd447a570435c1957a949452d2a8cd925def1d
Paul <paulbarmstrong@hotmail.com>
2019-12-24 00:57:51 -0500

    adding the video

--- src/App.js
+++ src/App.js
@@ -70,7 +70,13 @@ function App() {
 
 							{/* Blurb */}
 							<h4>About</h4>
-							<p>{XR_CONSTANTS.ABOUT_US_STR}</p>
+							<p>{XR_CONSTANTS.ABOUT_US_STR}</p><br/>
+							
+							{/* Video */}
+							<div style={{'padding':'56.25% 0 0 0','position':'relative'}}>
+								<iframe title="XR Terrestrial Video" src="https://player.vimeo.com/video/379079878?title=0&portrait=0" style={{'position':'absolute','top':'0','left':'0','width':'100%','height':'100%'}} frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+							</div>
+							<script src="https://player.vimeo.com/api/player.js"></script>
 						</div>
 					</div>
 				</li>
