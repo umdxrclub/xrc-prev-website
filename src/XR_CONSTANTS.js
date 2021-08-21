ac5ba1ad4e301060e3e88a4ebccb96e5978cd008
Sahil Mayenkar <sahil.mayenkar@gmail.com>
2021-08-21 19:24:00 -0400

    Updated with initial Fall 2021 info

--- src/XR_CONSTANTS.js
+++ src/XR_CONSTANTS.js
@@ -25,75 +25,40 @@ module.exports = Object.freeze({
 	LEADERSHIP_INFORMATION : [
 		{
 			'name' : 'Sahil Mayenkar',
-			'title' : 'President',
-			'github' : 'https://github.com/SahilMayenkar',
+			'title' : 'President, Lead Organizer of RealityHacks',
+			'web' : 'https://www.sahilmayenkar.com/',
 			'linkedin' : 'https://www.linkedin.com/in/sahil-mayenkar/',
+			'github' : 'https://github.com/SahilMayenkar'
 		},
 		{
-			'name' : 'Irene Li',
+			'name' : 'Jason Fotso',
 			'title' : 'Vice President',
-			'linkedin' : 'https://www.linkedin.com/in/irene-li-5a33a4147/',
+			'linkedin' : 'https://www.linkedin.com/in/jason-fotso/'
 		},
 		{
-			'name' : 'Anders Julin',
-			'title' : 'Treasurer',
-			'linkedin' : 'https://www.linkedin.com/in/andersjulin',
+			'name' : 'Andrew Yuan',
+			'title' : 'Treasurer, Mentor',
+			'web' : 'https://andrewyuantw.github.io/',
+			'linkedin' : 'https://www.linkedin.com/in/andrewyuantw/',
+			'github' : 'https://github.com/andrewyuantw'
 		},
 		{
-			'name' : 'Jordan Woo',
-			'title' : 'Lead Mentor',
-			'linkedin' : 'https://www.linkedin.com/in/minersail/',
-			'github' : 'https://github.com/minersail',
+			'name' : 'Rohan Bajpai',
+			'title' : 'Event Coordinator, Web Developer',
+			'linkedin' : 'https://www.linkedin.com/in/rohan-bajpai-20/',
+			'github' : 'https://github.com/rohannbajpai'
 		},
 		{
-			'name' : 'Matt Graber',
-			'title' : 'XR Mentor',
-			'github' : 'https://github.com/grabermtw',
-			'linkedin' : 'https://www.linkedin.com/in/matthew-william-graber-umd/',
-			'web' : 'https://devpost.com/mgraber1',
-		},
-		{
-			'name' : 'John Ball',
-			'title' : 'XR Mentor',
-			'linkedin' : 'https://www.linkedin.com/in/john-ball-337231186/',
-			'web' : 'https://devpost.com/jlball',
-			'web2' : 'https://jlballportfolio.wordpress.com/',
-		},
-		{
-			'name' : 'Jesse Parreira',
-			'title' : '3D Design Mentor, Snapchat Lens Studio Expert',
-			'linkedin' : 'https://www.linkedin.com/in/jesse-parreira-48aa44163/',
-			'twitter' : 'https://twitter.com/VeryNiceJokes',
-		},
-		{
-			'name' : 'James Biggins',
-			'title' : 'XR Mentor',
-			'web' : 'https://jabiggins.wixsite.com/website',
-			'linkedin' : 'https://www.linkedin.com/in/jbigginscs/',
-		},
-		{
-			'name' : 'Omer Bowman',
-			'title' : 'XR Mentor',
-			'linkedin' : 'https://www.linkedin.com/in/omer-bowman/',
-			'web' : 'https://mechanicallinguistics.wordpress.com',
-		},
-		{
-			'name' : 'Arun Kumar',
-			'title' : 'XR Mentor',
-			'linkedin' : 'https://www.linkedin.com/in/arun-kumar-209a02125/',
-		},
-		{
-			'name' : 'Sharon Zheng',
-			'title' : 'XR Mentor',
-		},
-		{
-			'name' : 'Shubham Mittal',
-			'title' : 'XR Mentor',
-			'linkedin' : 'https://www.linkedin.com/in/smittal3',
+			'name' : 'Mrugesh Shah',
+			'title' : 'Lab Manager'
 		},
 		{
 			'name' : 'Kyle Gerry',
-			'title' : 'XR Mentor',
+			'title' : 'Mentor'
+		},
+		{
+			'name' : 'Kyuyoung Kim',
+			'title' : 'Mentor'
 		}
 	],
 	
@@ -117,20 +82,11 @@ module.exports = Object.freeze({
 	// fa_icon should be a valid font-awesome icon name, don't include an fa_icon entry if you don't want an icon
 	EVENTS_INFORMATION : [
 		{
-			'name' : 'Spring 2021 Virtual Kickoff Event',
-			'date' : new Date(2021, 1, 12),
-			'time' : '6:30pm - 7:30pm',
-			'location' : 'Online via Zoom',
-			'fa_icon' : 'fa-bullhorn',
-			'url' : 'https://umd.zoom.us/j/99187307055',
-		},
-		{
-			'name' : 'Intro to Unity with the LEGO® Microgame',
-			'date' : new Date(2021, 1, 19),
-			'time' : '6:30pm - 7:45pm',
-			'location' : 'Online via Zoom',
-			'fa_icon' : 'fa-flask',
-			'url' : 'https://umd.zoom.us/j/98320595395',
+			'name' : 'Fall 2021 Kickoff Event',
+			'date' : new Date(2021, 8, 24),
+			'time' : 'Time: TBD',
+			'location' : 'Location: TBD',
+			'fa_icon' : 'fa-bullhorn'
 		}
 	],
 });
